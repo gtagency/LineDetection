0defb85181ed3d5a92b931134dec4eb0596b9440
Jesse Rosalia <jesse.rosalia@gatech.edu>
2013-10-20 14:39:55 -0400

    finished port of curve detection hid debug output behind a DEBUG define added capability to test with binarized image

--- cpp/curve_detect.cpp
+++ cpp/curve_detect.cpp
@@ -8,9 +8,11 @@
 using namespace cd;
 using namespace cv;
 
+// #define DEBUG
+
 CurveDetect::CurveDetect(int bh, int bw)
     : boxHeight(bh), boxWidth(bw) {
-    
+    A = (Mat_<float>(2,2) << 1, 0, 0, -1);
 }
 
 /*
@@ -69,7 +71,7 @@ void CurveDetect::canny(const Mat& src, Mat& edges) {
     int edgeThresh = 1;
     double low_thres = 30; //120;
     int const max_lowThreshold = 200;
-    int ratio = 2.7;
+    int ratio = 3;
     int kernel_size = 3;
     cv::Mat detected_edges;
     
@@ -85,8 +87,9 @@ void CurveDetect::canny(const Mat& src, Mat& edges) {
 
     /// Using Canny's output as a mask, we display our result
     edges = cv::Scalar::all(0);
+#ifdef DEBUG
     printf("Detected Size: %d %d\n", detected_edges.rows, detected_edges.cols);
-
+#endif
     src.copyTo( edges, detected_edges);
     // edges = src;
 }
@@ -94,14 +97,17 @@ void CurveDetect::canny(const Mat& src, Mat& edges) {
 /**
  * Rotate an image
  */
-void CurveDetect::rotate(const cv::Mat& src, Point2f center, double angleRad, cv::Mat& dst) {
+void CurveDetect::rotate(const cv::Mat& src, Point center, double angleRad, cv::Mat& dst) {
+#ifdef DEBUG
+    printf("Rotate image: %f radians\n", angleRad);
+#endif
     int len = std::max(src.cols, src.rows);
     cv::Mat r = cv::getRotationMatrix2D(center, angleRad * 180.0/M_PI, 1.0);
 
     cv::warpAffine(src, dst, r, cv::Size(len, len));
 }
 
-Rect CurveDetect::getROIRect(const Point2f& pt) {
+Rect CurveDetect::getROIRect(const Point& pt) {
     //TODO: may have to be negative or something
     float x = fmax(0, pt.x - boxWidth/2);
     float y = fmax(0, pt.y - boxHeight);
@@ -109,23 +115,36 @@ Rect CurveDetect::getROIRect(const Point2f& pt) {
     return Rect(x, y, boxWidth, boxHeight);
 }
 
+Rect CurveDetect::getTileRect(const Point& pt) {
+    //TODO: may have to be negative or something
+    float x = fmax(0, pt.x - boxWidth * 2);
+    float y = fmax(0, pt.y - boxHeight * 4);
+    printf("%f, %f, %d, %d\n", x, y, boxWidth * 4, boxHeight * 4);
+    return Rect(x, y, boxWidth * 4, boxHeight * 4);
+}
+
 //points is a n x 2 matrix, with x in first column and y in the second column
 void CurveDetect::getPointsOfInterest(const Mat& roi, Mat& points) {
+#ifdef DEBUG
+    printf("Get points of interest\n");
+#endif
     Mat edges;
     canny(roi, edges);
     Mat locations;   // output, locations of non-zero pixels 
     //locations should be a n x 1 vector of points
     findNonZero(edges, locations);
+#ifdef DBEUG
     printf("Found edges: %d, %d\n", locations.rows, locations.cols);
-    namedWindow( "Display window", CV_WINDOW_AUTOSIZE );// Create a window for display.
-        imshow( "Display window", edges );                   // Show our image inside it.
-        waitKey(0);
+    debugShowImage(edges);
+#endif
     //adjust the points so they're relative to axes at the bottom center of the box.
     points = Mat::zeros(locations.rows, 2, CV_32FC1);
     for (int r = 0; r < locations.rows; r++) {
         Point *locPtr = locations.ptr<Point>(r);
         float *ptPtr  = points.ptr<float>(r);
+#ifdef DEBUG
         printf("%d, %d\n", locPtr[0].x, locPtr[0].y);
+#endif
         ptPtr[0] = locPtr[0].x;
         ptPtr[1] = locPtr[0].y;
     } 
@@ -133,136 +152,151 @@ void CurveDetect::getPointsOfInterest(const Mat& roi, Mat& points) {
 
 Mat CurveDetect::polypoint(const Mat& poly, float x) {
     //TODO: currently only works with 1 degree polynomial...can expand to n degree using varargs
+#ifdef DEBUG
     printf("Poly: %f, %f, x: %f\n", poly.at<float>(0), poly.at<float>(1), x);
+#endif 
     return (Mat_<float>(2,1) << x, poly.at<float>(0) + x * poly.at<float>(1));
 }
 
+Mat CurveDetect::getBestPointInImage(const Mat& bottom, const Mat& top, const Mat& imageStart, double rotation) {
+    Mat Rt = (Mat_<float>(2,2) << cos(rotation), -sin(rotation),
+                                  sin(rotation),  cos(rotation));
+    Rt = Rt.t();
+                              
+    Mat adjTop = A * Rt * A * top + imageStart;
+    Mat adjBot = A * Rt * A * bottom + imageStart;
+#ifdef DEBUG
+    printf("%f, %f\n", adjTop.at<float>(0), adjTop.at<float>(1));
+    printf("%f, %f\n", adjBot.at<float>(0), adjBot.at<float>(1));
+#endif
+    Mat nextPt = adjTop;
+    if (norm(imageStart - adjTop) < norm(imageStart - adjBot)) {
+        nextPt = adjBot;
+    }
+#ifdef DEBUG
+    printf("%f, %f\n", imageStart.at<float>(0), imageStart.at<float>(1));
+    printf("%f, %f\n", nextPt.at<float>(0), nextPt.at<float>(1));
+#endif
+    return nextPt;
+}
+
+void CurveDetect::fitPoly(const Mat& points, Mat& bottom, Mat& top, float& lineAngle) {
+    //fit a y,x polynomial, so we can compute x values for the top and bottom
+    // of the box
+    Mat xsMat = points.col(0) - boxWidth/2;
+    Mat ysMat = points.col(1) - boxHeight;
+    Mat poly;
+    poly = Mat::zeros(2, 1,CV_32FC1);
+#ifdef DEBUG
+    printf("%d, %d, %d, %d, %d, %d, %d, %d, %d\n", xsMat.rows, xsMat.cols, xsMat.type(), ysMat.rows, ysMat.cols, ysMat.type(), poly.rows, poly.cols, poly.type());
+    for (int r = 0; r < xsMat.rows; r++) {
+        printf("%f, %f\n", xsMat.ptr<float>(r)[0], ysMat.ptr<float>(r)[0]);
+    } 
+#endif
+    //this returns a polynomial in poly, in the right order
+    // but remember, this is a y,x polynomial, so poly(0) is x intercept
+    polyfit(ysMat, xsMat, poly, 1);
+
+    //these points are x,y, due to call to flip
+    top = polypoint(poly, -boxHeight);
+    flip(top, top, -1);
+
+    bottom = polypoint(poly, 0);
+    flip(bottom, bottom, -1);
+
+    lineAngle = tan(poly.at<float>(1));
+#ifdef DEBUG
+    printf("Polynomial: %f, %f\n", poly.at<float>(0), poly.at<float>(1));
+    
+    printf("DEBUG testing the poly points\n");
+    printf("Top of roi: %f, %f\n", top.at<float>(0), top.at<float>(1));
+    printf("Bottom of roi: %f, %f\n", bottom.at<float>(0), bottom.at<float>(1));
+#endif
+}
+
+void CurveDetect::debugShowImage(const Mat& img) {
+#ifdef DEBUG
+    namedWindow( "Display window", CV_WINDOW_AUTOSIZE );// Create a window for display.
+    imshow( "Display window", img );                   // Show our image inside it.
+    waitKey(0);
+#endif
+}
+
 //NOTE: assumes src is a binary image
-void CurveDetect::fitCurve(const Mat& src, const Point2f& startPt, int maxCurves) {
+void CurveDetect::fitCurve(const Mat& src, const Point& startPt, int maxCurves) {
     int curves = 0;
     Size size = src.size();
+#ifdef DEBUG
     printf("Source Size: %d %d\n", src.rows, src.cols);
     //TODO: preprocess image? convert color, etc
     printf("Detecting best initial theta\n");
+#endif
     Mat roi = Mat(src, getROIRect(startPt));
     //NOTE: this assumes that the curve is at least as big as one box in width and length
     // also assumes width, height >= 2
-    double theta0 = M_PI/2.7;//findBestTheta(roi, Point(boxWidth/2, boxHeight/2), fmin(boxWidth/2, boxHeight/2));
+    float theta0 = M_PI/2.7;//findBestTheta(roi, Point(boxWidth/2, boxHeight/2), fmin(boxWidth/2, boxHeight/2));
+
+#ifdef DEBUG
     printf("Best theta: %.02f", theta0);
     printf("Start curve fitting\n");
-    std::vector<Point> points;
+#endif
+    //clear the current vectors
+    points.clear();
+    thetas.clear();
 
-    Mat A = (Mat_<float>(2,2) << 1, 0, 0, -1);
-
-    // Mat can;
-    // canny(src, can);
-    
-    // namedWindow( "Display window", CV_WINDOW_AUTOSIZE );// Create a window for display.
-    //     imshow( "Display window", can );                   // Show our image inside it.
-    // 
-    //     waitKey(0);
     points.push_back(startPt);
-    Point2f pt = startPt;
+    thetas.push_back(theta0);
+
+    Point pt = startPt;
+
+#ifdef DEBUG    
     Mat show = src.clone();
+#endif
     
-    double theta = theta0;
+    float theta = theta0;
     while (pt.x < size.width && pt.y < size.height && curves < maxCurves) {
-        printf("%f\n", theta);
         float rotation = M_PI/2 - theta;
-        printf("Rotate image: %f radians\n", rotation);
+        //get the region of interest...the region we're looking at
+        // Mat tile = Mat(src, getTileRect(pt));
+        // debugShowImage(tile);
         Mat rot;
+        // rotate(tile, Point(boxWidth, boxHeight), rotation, rot);
         rotate(src, pt, rotation, rot);
-        namedWindow( "Display window", CV_WINDOW_AUTOSIZE );// Create a window for display.
-            imshow( "Display window", rot );                   // Show our image inside it.
-            waitKey(0);
-        printf("Extract ROI\n");
+        debugShowImage(rot);
+
+        // roi = Mat(rot, getROIRect(Point(tile.size().width - boxWidth/4, tile.size().height - boxHeight / 2)));
         roi = Mat(rot, getROIRect(pt));
-        printf("Get points of interest\n");
-        Mat points;
-        namedWindow( "Display window", CV_WINDOW_AUTOSIZE );// Create a window for display.
-            imshow( "Display window", roi );                   // Show our image inside it.
-            waitKey(0);
-        getPointsOfInterest(roi, points);
-        // Mat edges;
-        // canny(roi, edges);
-        // Mat locations;   // output, locations of non-zero pixels 
-        // findNonZero(edges, locations);
-        // printf("Found edges: %d, %d\n", locations.rows, locations.cols);
-        //     
-        // //adjust the points so they're relative to axes at the bottom center of the box.
-        // std::vector<float> xs;
-        // std::vector<float> ys;
-        // for (int r = 0; r < locations.rows; r++) {
-        //     Point *ptr = locations.ptr<Point>(r);
-        //     for (int c = 0; c < locations.cols; c++) {
-        //         printf("%d, %d\n", ptr[c].x, ptr[c].y);
-        //         xs.push_back(ptr[c].x - boxWidth / 2);
-        //         ys.push_back(ptr[c].y - boxHeight);
-        //     }
-        // } 
-        
-        // for (int ii = 0; ii < xs.size(); ii++) {
-        //     printf("%f, %f\n", xs[ii], ys[ii]);
-        // }
-        // 
-        
-        //fit a y,x polynomial, so we can compute x values for the top and bottom
-        // of the box
-        Mat poly;
-        Mat xsMat = points.col(0) - boxWidth/2;
-        Mat ysMat = points.col(1) - boxHeight;
-        poly = Mat::zeros(2, 1,CV_32FC1);
-        printf("%d, %d, %d, %d, %d, %d, %d, %d, %d\n", xsMat.rows, xsMat.cols, xsMat.type(), ysMat.rows, ysMat.cols, ysMat.type(), poly.rows, poly.cols, poly.type());
-        for (int r = 0; r < xsMat.rows; r++) {
-            printf("%f, %f\n", xsMat.ptr<float>(r)[0], ysMat.ptr<float>(r)[0]);
-        } 
-        polyfit(ysMat, xsMat, poly, 1);
-        //so we're in order c(0) + c(1)x + c(2)x^2...
-        // flip(poly, poly, -1);
-        printf("Polynomial: %f, %f\n", poly.at<float>(0), poly.at<float>(1));
-        
-        //these points are y,x
-        Mat p1 = polypoint(poly, -boxHeight);
-        Mat p2 = polypoint(poly, 0);
+        debugShowImage(roi);
 
-        printf("DEBUG testing the poly points\n");
-        printf("%f, %f\n", p1.at<float>(0), p1.at<float>(1));
-        printf("%f, %f\n", p2.at<float>(0), p2.at<float>(1));
+        Mat poi;
+        //get points of interest to consider when fitting the line
+        getPointsOfInterest(roi, poi);
 
-        Mat Rt = (Mat_<float>(2,2) << cos(rotation), -sin(rotation),
-                                      sin(rotation),  cos(rotation));
-        Rt = Rt.t();
-                                  
-        // printf("%f, %f\n", x1, x2);
-        flip(p1, p1, -1);
-        flip(p2, p2, -1);
+        float lineAngle;
+        Mat bottom, top;
+        //fit a line to these points, and compute the points on this line
+        // at the bottom and top of the region of interest 
+        fitPoly(poi, bottom, top, lineAngle);
+        
         Mat ptMat = (Mat_<float>(2,1) << pt.x, pt.y);
-        p1 = A * Rt * A * p1 + ptMat;
-        p2 = A * Rt * A * p2 + ptMat;
 
-        printf("%f, %f\n", p1.at<float>(0), p1.at<float>(1));
-        printf("%f, %f\n", p2.at<float>(0), p2.at<float>(1));
+        //compute
+        Mat nextPtMat = getBestPointInImage(bottom, top, ptMat, rotation);
+        Point nextPt  = Point(nextPtMat.at<float>(0), nextPtMat.at<float>(1));
 
-        Mat nextPt = p1;
-        if (norm(ptMat - p1) < norm(ptMat - p2)) {
-            nextPt = p2;
-        }
+#ifdef DEBUG
+        line(show, pt, nextPt, Scalar(127, 127, 127));
+        debugShowImage(show);
+        printf("%f, %f\n", lineAngle, theta);
+#endif
+        pt = nextPt;
+        theta = lineAngle + theta;
 
-        printf("%f, %f\n", pt.x, pt.y);
-        printf("%f, %f\n", nextPt.at<float>(0), nextPt.at<float>(1));
-        
-        Point pt2 = Point(nextPt.at<float>(0), nextPt.at<float>(1));
-        line(show, pt, pt2, Scalar(127, 127, 127));
-        namedWindow( "Display window", CV_WINDOW_AUTOSIZE );// Create a window for display.
-            imshow( "Display window", show );                   // Show our image inside it.
-        pt = pt2;
-        printf("%f, %f\n", atan(poly.at<float>(1)), theta);
-        theta = atan(poly.at<float>(1)) + theta;
-        printf("%f, %f\n", pt.x, pt.y);
-        // dst = W.clone(); do cv::flip(W,dst,-1);
+        points.push_back(pt);
+        thetas.push_back(theta);
         curves++;
-        waitKey(0);
     }
+    
 //     points = zeros(2,4);
 //     thetas = zeros(1,4);
 //     p = p0;
@@ -275,6 +309,13 @@ void CurveDetect::fitCurve(const Mat& src, const Point2f& startPt, int maxCurves
 //     end
 //     points(:,i+1) = p;
 //     thetas(i+1) = theta;
+}
+
+std::vector<Point>& CurveDetect::getPointsOnCurve() {
+    return this->points;
+}
+std::vector<float>& CurveDetect::getAnglesOfIncidence() {
+    return this->thetas;
 }
 
 Scalar LOW_HSV = Scalar(60, 50, 50);
@@ -294,11 +335,27 @@ void getBinary(Mat& src, Scalar& low_HSV, Scalar& hi_HSV, Mat& dest) {
 
 
 int main(int argc, const char **argv) {
-    Mat src = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+    Mat src = imread(argv[1], CV_LOAD_IMAGE_COLOR);
     Mat bin = src;
-    // getBinary(src, LOW_HSV, HIGH_HSV, bin);
+    getBinary(src, LOW_HSV, HIGH_HSV, bin);
     
     CurveDetect cd = CurveDetect(40, 20);
-    Point2f start = Point2f(350, 705);
+    Point start = Point(350, 705);
     cd.fitCurve(bin, start, 15);
+    
+    Mat show = src.clone();
+    Point pt = start;
+    for (std::vector<Point>::iterator it = cd.getPointsOnCurve().begin();
+         it != cd.getPointsOnCurve().end();
+         it++) {
+         printf ("%d, %d, %d, %d\n", pt.x, pt.y, it->x, it->y);
+        line(show, pt, *it, Scalar(127, 127, 127));
+        pt = *it;
+    }
+
+    namedWindow( "Display window", CV_WINDOW_AUTOSIZE );// Create a window for display.
+    imshow( "Display window", show );                   // Show our image inside it.
+    waitKey(0);
+    
+    
 }
